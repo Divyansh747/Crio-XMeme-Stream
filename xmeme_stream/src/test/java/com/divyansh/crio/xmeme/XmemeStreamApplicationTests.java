06dd9ea2d8cd5e38047d0abd644f1e5cf98c590d
divyansh747 <divyansh.1999.dr@gmail.com>
2021-02-05 01:21:30 +0530

    Spring boot project initial push

---
+++ xmeme_stream/src/test/java/com/divyansh/crio/xmeme/XmemeStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.crio.xmeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmemeStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
