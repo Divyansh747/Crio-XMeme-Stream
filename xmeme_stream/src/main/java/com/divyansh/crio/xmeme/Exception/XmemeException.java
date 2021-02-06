a04de7c1027b01f973449359f9f539805e8219f9
divyansh747 <divyansh.1999.dr@gmail.com>
2021-02-06 20:15:15 +0530

    create XmemeException to handle Exceptions

---
+++ xmeme_stream/src/main/java/com/divyansh/crio/xmeme/Exception/XmemeException.java
@@ -0,0 +1,16 @@
+package com.divyansh.crio.xmeme.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class XmemeException extends RuntimeException {
+
+	public XmemeException(String error) {
+		super(error);
+	}
+
+	public XmemeException(String error, Throwable cause) {
+		super(error, cause);
+	}
+}
