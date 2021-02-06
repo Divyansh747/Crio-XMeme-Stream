b299871e4751bc1db4231523e31f392090ae31e2
divyansh747 <divyansh.1999.dr@gmail.com>
2021-02-07 02:18:41 +0530

    change Exception error to ERROR 409

--- xmeme_stream/src/main/java/com/divyansh/crio/xmeme/Exception/XmemeException.java
+++ xmeme_stream/src/main/java/com/divyansh/crio/xmeme/Exception/XmemeException.java
@@ -3,7 +3,7 @@ package com.divyansh.crio.xmeme.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class XmemeException extends RuntimeException {
 
 	public XmemeException(String error) {
